7bd0de2c564d9e875656128981b170ae1d0b7c0f
Jason Schadel <jason.schadel@gmail.com>
2015-11-08 09:53:44 -0500

    Add tests to ensure jenkins is running

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe 'sparkins_jenkins::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+
+  describe service('jenkins') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port(8080) do
+    it { should be_listening }
   end
 end
