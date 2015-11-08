fe65fdca7663286defea2962a5be87faacb1b6fb
Jason Schadel <jason.schadel@gmail.com>
2015-11-08 08:15:20 -0500

    Updated Policyfile and created lock

--- Policyfile.rb
+++ Policyfile.rb
@@ -4,13 +4,14 @@
 # https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
 
 # A name that describes what the system you're building with Chef does.
-name "example-application-service"
+name "sparkins_jenkins"
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list "example_cookbook::default"
+run_list "sparkins_jenkins::default"
 
 # Specify a custom source for a single cookbook:
 # cookbook "example_cookbook", path: "../cookbooks/example_cookbook"
+cookbook "sparkins_jenkins", path: "cookbooks/sparkins_jenkins"
