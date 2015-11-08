4a94d534c3e58b28ac24b00045117b8e80026924
Jason Schadel <jason.schadel@gmail.com>
2015-11-08 08:12:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# sparkins_jenkins
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/sparkins_jenkins/metadata.rb
@@ -0,0 +1,7 @@
+name 'sparkins_jenkins'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures sparkins_jenkins'
+long_description 'Installs/Configures sparkins_jenkins'
+version '0.1.0'
