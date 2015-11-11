60f3b8d77e37579c7ef28b9665b7886bed2516da
Jason Schadel <jason.schadel@gmail.com>
2015-11-10 22:20:57 -0500

    Add the email-ext jenkins plugin

--- cookbooks/sparkins_jenkins/recipes/default.rb
+++ cookbooks/sparkins_jenkins/recipes/default.rb
@@ -7,8 +7,10 @@ include_recipe 'jenkins::master'
 
 include_recipe 'git'
 
-jenkins_plugin 'git' do
-  notifies :restart, 'service[jenkins]', :immediately
+%w(git email-ext).each do |plugin|
+  jenkins_plugin plugin do
+    notifies :restart, 'service[jenkins]', :immediately
+  end
 end
 
 sparkins_config_filename = File.join(Chef::Config[:file_cache_path], 'sparkins-config.xml')
--- cookbooks/sparkins_jenkins/spec/unit/recipes/default_spec.rb
+++ cookbooks/sparkins_jenkins/spec/unit/recipes/default_spec.rb
@@ -29,6 +29,10 @@ describe 'sparkins_jenkins::default' do
       expect(chef_run).to install_jenkins_plugin('git')
     end
 
+    it 'should install the email-ext plugin' do
+      expect(chef_run).to install_jenkins_plugin('email-ext')
+    end
+
     it 'should notify jenkins to restart' do
       git_plugin = chef_run.jenkins_plugin('git')
       expect(git_plugin).to notify('service[jenkins]')
