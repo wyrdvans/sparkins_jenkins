7213fdca424224d22ac257e58035e954e56d877e
Jason Schadel <jason.schadel@gmail.com>
2015-11-10 23:50:46 -0500

    Add serverspec tests for email service

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,12 +2,25 @@ require 'spec_helper'
 
 describe 'sparkins_jenkins::default' do
 
-  describe service('jenkins') do
-    it { should be_enabled }
-    it { should be_running }
+  context 'Jenkins should be configured' do
+    describe service('jenkins') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+    
+    describe port(8080) do
+      it { should be_listening }
+    end
   end
 
-  describe port(8080) do
-    it { should be_listening }
+  context 'Email should be configured' do
+    describe service('postfix') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+    
+    describe port(25) do
+      it { should be_listening }
+    end
   end
 end
